b83613abf4e558d46143b3f0ea736c9c6a2ff84a
Suraj Yadav <sy083609@gmail.com>
2025-08-12 12:08:34 +0530

    suraj

--- server/src/controllers/healthcheck.controller.ts
+++
@@ -1,9 +0,0 @@
-import { asyncHandler } from "../utils/asyncHandler.js";
-import { ApiResponse } from "../utils/ApiResponse.js";
-import { Request, Response } from "express";
-
-const healthcheck = asyncHandler(async (req: Request, res: Response) => {
-  return res.status(200).json(new ApiResponse(200, {}, "OK"));
-});
-
-export { healthcheck };
